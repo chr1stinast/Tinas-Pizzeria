ffcd264042854f1c6d4275d4ef497fb2b0cb0dac
CHRISTINA TASHJI <chrtas24@bergen.org>
2022-01-18 08:50:19 -0500

    matt mika christina liveshare session

---
+++ Overworld.js
@@ -0,0 +1,22 @@
+class Overworld {
+    constructor(config) {
+        this.element = config.element;
+        this.canvas = this.element.querySelector(".game-canvas");
+        this.ctx = this.canvas.getContext("2d");
+    }
+
+    init() {
+        const image = new Image();
+        image.onload = () => {
+            this.ctx.drawImage(image,0,0);
+        }
+        image.src = "/images/maps/DemoLower.png";
+
+        const mc = new Image();
+        mconsole.onload = () => {
+            
+        }
+
+
+    }
+}
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 (function () {
-    console.log("woo")
+    const overworld = new Overworld({
+        element: document.querySelector(".game-container")
+    });
+    overworld.init();
 })();
