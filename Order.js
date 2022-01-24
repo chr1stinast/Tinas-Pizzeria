34b24ab662e71d9db1aa13e865228415cf1edbbc
Your Name <you@example.com>
2022-01-24 10:43:23 -0500

    .

--- Order.js
+++ Order.js
@@ -1,5 +1,10 @@
 class Order {
-    // order class that stores information for each order
+    // order class that stores information for each order:
+    // - order number/info
+    // - topping station
+    // - baking station
+    // - cutting station
+    // - score for respective stations
     constructor(topping, num, orderNumber) {
         this.topping = topping;
         this.num = num;
@@ -9,4 +14,5 @@ class Order {
     displayInfo() {
         console.log("Order #" + this.orderNumber + ": " + this.topping + " pizza with " + this.num + " on it.");
     }
+    // method to take input (player activity), and compares it with order info then calculates + displays score while also updating player.js
 }
