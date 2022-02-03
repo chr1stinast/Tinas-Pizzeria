33ac294d5b62f8e05a5a38fef807a97722b9af71
MATTHEW DIOMIDOUS <mattdiom@gmail.com>
2022-02-03 08:40:05 -0500

    title screen (not complete yet)

---
+++ TitleScreen.js
@@ -0,0 +1,41 @@
+class TitleScreen {
+    constructor({ progress }) {
+        this.progress = progress;
+    }
+
+    getOptions() {
+        return[
+            {
+                label: "New Game",
+                description: "Start a new pizza adventure!",
+                handler: () => {
+                    this.close();
+                    resolve();
+                }
+            }
+        ]
+    }
+    
+    createElement() {
+        this.element = document.createElement("div");
+        this.element.classList.add("TitleScreen");
+        this.element.innerHTML = (`
+            <img class="TitleScreen_logo" src="/images/logo.png" alt="Papas-Games-Remake" />
+        `)
+    }
+    
+    close() {
+        this.keyboardMenu.end();
+        this.element.remove();
+    }
+
+    init(container) {
+        return new Promise(resolve => {
+            this.createElement();
+            container.appendChild(this.element);
+            this.keyboardMenu = new KeyboardMenu();
+            this.keyboardMenu.init(this.element);
+            this.keyboardMenu.setOptions(this.getOptions(resolve));
+        })
+    }
+}
