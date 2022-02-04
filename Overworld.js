5010355796a7e1c751f74414682ad35d9edc6f2b
CHRISTINA TASHJI <chrtas24@bergen.org>
2022-02-04 08:02:53 -0500

    fixed brackets error mess

--- Overworld.js
+++ Overworld.js
@@ -94,55 +94,50 @@ class Overworld {
      { type: "textMessage", text: "Mika Be is a flop!"}
    ])
   
-      const order1 = new Order("Pepperoni", 5, 60, 1);
-      order1.displayInfo();
-  
-      const order2 = new Order("Pepperoni", 6, 60, 2);
-      order2.displayInfo();
-      
-  
-      this.startGameLoop();
-      constructor(config) {
-        this.element = config.element;
-        this.canvas = this.element.querySelector(".game-canvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.map = null;
-      }
+    const order1 = new Order("Pepperoni", 5, 60, 1);
+    order1.displayInfo();
+
+    const order2 = new Order("Pepperoni", 6, 60, 2);
+    order2.displayInfo();
+    
+
+    this.startGameLoop();
+    }
    
-     startGameLoop() {
-       const step = () => {
-         //Clear off the canvas
-         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-   
-         //Establish the camera person
-         const cameraPerson = this.map.gameObjects.hero;
-   
-         //Update all objects
-         Object.values(this.map.gameObjects).forEach(object => {
-           object.update({
-             arrow: this.directionInput.direction,
-             map: this.map,
-           })
-         })
-   
-         //Draw Lower layer
-         this.map.drawLowerImage(this.ctx, cameraPerson);
-   
-         //Draw Game Objects
-         Object.values(this.map.gameObjects).sort((a,b) => {
-           return a.y - b.y;
-         }).forEach(object => {
-           object.sprite.draw(this.ctx, cameraPerson);
-         })
-   
-         //Draw Upper layer
-         this.map.drawUpperImage(this.ctx, cameraPerson);
-         
-         requestAnimationFrame(() => {
-           step();   
-         })
-       }
-       step();
+      startGameLoop() {
+        const step = () => {
+          //Clear off the canvas
+          this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    
+          //Establish the camera person
+          const cameraPerson = this.map.gameObjects.hero;
+    
+          //Update all objects
+          Object.values(this.map.gameObjects).forEach(object => {
+            object.update({
+              arrow: this.directionInput.direction,
+              map: this.map,
+            })
+          })
+    
+          //Draw Lower layer
+          this.map.drawLowerImage(this.ctx, cameraPerson);
+    
+          //Draw Game Objects
+          Object.values(this.map.gameObjects).sort((a,b) => {
+            return a.y - b.y;
+          }).forEach(object => {
+            object.sprite.draw(this.ctx, cameraPerson);
+          })
+    
+          //Draw Upper layer
+          this.map.drawUpperImage(this.ctx, cameraPerson);
+          
+          requestAnimationFrame(() => {
+            step();   
+          })
+        }
+        step();
     };
    
     bindActionInput() {
