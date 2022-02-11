82e981e7a56ea25f63bb07729212f7732f71d89a
Mika Be <mikushbe@gmail.com>
2022-02-11 08:29:26 -0500

    idk im working on it

--- ZTalkExp.js
+++ ZTalkExp.js
@@ -1,47 +1,68 @@
 class ZTalkExp {
-    constructor() {
-        let btn = document.createElement("button");
-        btn.innerHTML = "Save";
-        btn.onclick = function () {
-        alert("Button is clicked");
-        };
-    document.body.appendChild(btn);
-
+    constructor({onComplete, optionA, optionB, optionC}) {
+        this.onComplete = onComplete;
     }
 
     getOptions() {
-        return[
-            {
-                label: "Hi??",
-                description: "Start a new pizza adventure!",
-                handler: () => {
-                    this.close();
-                    resolve();
+        
+            return [
+                //All of our pizzas (dynamic)
+                //...lineupPizzas,
+                {
+                    label: optionA,
+                    description: "Save your progress",
+                    handler: () => {
+                        this.menuSubmit(action);
+                        this.close();
+                    }
+                },
+                {
+                    label: optionB,
+                    description: "Close the pause menu",
+                    handler: () => {
+                        this.close();
+                    }
+                },
+                {
+                    label: optionC,
+                    description: "Close the pause menu",
+                    handler: () => {
+                        this.close();
+                    }
                 }
-            }
-        ]
-    }
-    
-    createElement() {
-        this.element = document.createElement("div");
-        this.element.classList.add("TitleScreen");
-        this.element.innerHTML = (`
-            <img class="TitleScreen_logo" src="/images/logo.png" alt="Papas-Games-Remake" />
-        `)
-    }
-    
-    close() {
-        this.keyboardMenu.end();
-        this.element.remove();
+            ]
+        
     }
 
-    init(container) {
-        return new Promise(resolve => {
-            this.createElement();
-            container.appendChild(this.element);
-            this.keyboardMenu = new KeyboardMenu();
-            this.keyboardMenu.init(this.element);
-            this.keyboardMenu.setOptions(this.getOptions(resolve));
+    createElement() {
+        this.element = document.createElement("div");
+        this.element.classList.add("PauseMenu");
+        this.element.innerHTML = (`
+            <h2>Pause Menu</h2>
+        `)
+    }
+
+    close() {
+        this.esc?.unbind();
+        this.keyboardMenu.end();
+        this.element.remove();
+        this.onComplete();
+    }
+
+    async init(container) {
+        this.createElement();
+        this.keyboardMenu = new KeyboardMenu({
+            descriptionContainer: container
         })
+        this.keyboardMenu.init(this.element);
+        this.keyboardMenu.setOptions(this.getOptions("root"));
+        
+        container.appendChild(this.element);
+
+        // //escape key closes pause menu
+        // utils.wait(200);
+        // this.esc = new KeyPressListener("Escape", () => {
+        //     this.close();
+        // })
     }
 }
